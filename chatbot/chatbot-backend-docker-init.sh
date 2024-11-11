87b7d2ebd00372127ed2148129b86aea581c35b2
Nguyen Van Nam <namnv@younetgroup.com>
2024-11-11 15:49:52 +0700

    fix: update chatbot api

---
+++ chatbot/chatbot-backend-docker-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $CHATBOT_ENV == "api" ]]; then
+  if [[ $FLASK_ENV == "development" ]]; then
+    export FLASK_DEBUG=1
+    flask run --port=8505 --host="0.0.0.0"
+  else
+    export FLASK_DEBUG=0
+    export FLASK_ENV=production
+    gunicorn --workers 4 --bind 0.0.0.0:8502 "project:create_app()"
+  fi
+fi
+
+if [[ $CHATBOT_ENV == "socketio" ]]; then
+  gunicorn --workers=1 --worker-class=eventlet --bind=0.0.0.0:8503 wsgi:app
+fi
+
+if [[ $CHATBOT_ENV == "worker" ]]; then
+  celery -A make_celery worker -l info -E
+fi
+
+if [[ $CHATBOT_ENV == "scheduler" ]]; then
+  celery -A make_celery beat -l info -E
+fi
