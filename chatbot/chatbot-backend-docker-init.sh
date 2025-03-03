33ca663ac9ff98398582a9574d3742e3966d2b27
Jacky <15958678+jacky158@users.noreply.github.com>
2025-03-03 09:20:22 +0700

    Update chatbot-backend-docker-init.sh

--- chatbot/chatbot-backend-docker-init.sh
+++ chatbot/chatbot-backend-docker-init.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $CHATBOT_ENV == "api" ]]; then
-  if [[ $FLASK_ENV == "development" ]]; then
+if [ $CHATBOT_ENV = "api" ]; then
+  if [ $FLASK_ENV = "development" ]; then
     export FLASK_DEBUG=1
-    flask run --port=8502 --host="0.0.0.0"
+    flask run --port=8502 --extra-files=/app/project --host=0.0.0.0
   else
     export FLASK_DEBUG=0
     export FLASK_ENV=production
@@ -11,14 +11,14 @@ if [[ $CHATBOT_ENV == "api" ]]; then
   fi
 fi
 
-if [[ $CHATBOT_ENV == "socketio" ]]; then
+if [ $CHATBOT_ENV = "socketio" ]; then
   gunicorn --workers=1 --worker-class=eventlet --bind=0.0.0.0:8503 wsgi:app
 fi
 
-if [[ $CHATBOT_ENV == "worker" ]]; then
+if [ $CHATBOT_ENV = "worker" ]; then
   celery -A make_celery worker -l info -E
 fi
 
-if [[ $CHATBOT_ENV == "scheduler" ]]; then
+if [ $CHATBOT_ENV = "scheduler" ]; then
   celery -A make_celery beat -l info
 fi
